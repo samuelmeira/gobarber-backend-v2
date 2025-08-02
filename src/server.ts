4d33d43362e0030d9eed8505c780a4845e78af43
Samuel Meira <slmeira.dev@gmail.com>
2025-08-02 15:55:08 -0300

    First commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import routes from './routes';
+import 'reflect-metadata';
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log('🚀 Server is running at http://localhost:3000');
+});
