000b1437655fb910c6aff8c668c5659102bdb513
Samuel Meira <slmeira.dev@gmail.com>
2025-09-05 00:00:12 -0300

    created ResetPasswordService

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -12,11 +12,21 @@ class FakeUserTokensRepository implements IUserTokensRepository {
       id: uuid(),
       token: uuid(),
       user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
     });
 
     this.userTokens.push(userToken);
 
     return Promise.resolve(userToken);
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      findToken => token === findToken.token,
+    );
+
+    return userToken;
   }
 }
 
